696d57adaf3c9a64807e9b9afab5f2542bae0fbe
hayderjebur <49797771+hayderjebur@users.noreply.github.com>
2021-10-08 15:01:49 -0400

    Update user.js

--- src/user.js
+++ src/user.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  name: String
+const PointSchema =new Schema({
+  type:{type: String,default:'Point'},
+  coordinates:{ type: [Number], index: '2dsphere'}
+})
+
+
+const DeriverSchema = new Schema({
+  name: String,
+  geometry: PiontSchema
 });
 
 const User = mongoose.model("user", UserSchema);
